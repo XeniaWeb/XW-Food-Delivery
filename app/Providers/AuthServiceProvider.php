bd16159f646c8e60f120bbaf392a73b8b0b396f8
XeniaWeb <xenia1898@gmail.com>
2023-12-02 18:23:38 +0100

    add Role & Permission

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
+use App\Models\Permission;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -21,6 +22,19 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerGates();
+    }
+
+    protected function registerGates()
+    {
+        try {
+            foreach (Permission::pluck('name') as $permission) {
+                Gate::define($permission, function ($user) use ($permission) {
+                    return $user->hasPermission($permission);
+                });
+            }
+        }catch (\Exception $e) {
+            info('registerPermissions(): Database not found or not yet migrated. Ignoring user permissions while booting app.');
+        }
     }
 }
